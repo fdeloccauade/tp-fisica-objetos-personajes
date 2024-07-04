f5a7a418597fefe5df556b5bfc02a9d7ea24116c
Gabriel Omar Guerrero <guerrerogabriel1999@gmail.com>
2024-07-04 20:40:30 -0300

    Add shooterrr game
    gggggg

--- src/main/sketch.js
+++ src/main/sketch.js
@@ -1,22 +1,24 @@
 import { FootballGame } from '../football/FootballGame.js';
 import { AirHockeyGame } from '../air_hockey/AirHockeyGame.js';
+import { ShooterGame } from '../shooter/ShooterGame.js';
 
 new p5(p => {
     let footballGame = new FootballGame(p);
     let airHockeyGame = new AirHockeyGame(p);
+    let shooterGame = new ShooterGame(p);
     let game = 0;
     let menuActive = true;
     
     p.setup = function() {
         p.createCanvas(800, 400);
 
-        p.circleX = (p.width  / 2) + 200;
+        p.circleX = (p.width / 2) + 200;
         p.circleY = p.height / 2;
         
-        p.circleAirHockeyX = (p.width  / 2) - 200;
+        p.circleAirHockeyX = (p.width / 2) - 200;
         p.circleAirHockeyY = p.height / 2;
 
-        p.circleShooterX = (p.width  / 2);
+        p.circleShooterX = (p.width / 2);
         p.circleShooterY = p.height / 2 - 150;
 
         p.dotSize = 10;
@@ -26,57 +28,62 @@ new p5(p => {
     p.draw = function() {
         p.background(255);
 
-        p.fill(220);
-        p.point(p.mouseX, p.mouseY, p.dotSize, p.dotSize);
-        p.ellipse(p.circleX, p.circleY, p.circleSize, p.circleSize);
-        p.ellipse(p.circleShooterX, p.circleShooterY, p.circleSize, p.circleSize);
-        p.rect(p.circleAirHockeyX - p.circleSize / 2, p.circleAirHockeyY - p.circleSize / 2, p.circleSize, p.circleSize);
+        if (menuActive) {
+            p.fill(220);
+            p.ellipse(p.circleX, p.circleY, p.circleSize, p.circleSize);
+            p.ellipse(p.circleShooterX, p.circleShooterY, p.circleSize, p.circleSize);
+            p.rect(p.circleAirHockeyX - p.circleSize / 2, p.circleAirHockeyY - p.circleSize / 2, p.circleSize, p.circleSize);
 
-        p.textAlign(p.CENTER, p.CENTER);
-        p.textSize(32);
-        p.fill(0);
-        p.text("FOOTBALL", p.circleX, p.circleY);
-        p.fill(0);
+            p.textAlign(p.CENTER, p.CENTER);
+            p.textSize(32);
+            p.fill(0);
+            p.text("FOOTBALL", p.circleX, p.circleY);
+            p.text("AIR HOCKEY", p.circleAirHockeyX, p.circleAirHockeyY);
+            p.text("SHOOTER", p.circleShooterX, p.circleShooterY);
+            
+            // Check for clicks on the menu items
+            let distanceToFootballCircle = p.dist(p.mouseX, p.mouseY, p.circleX, p.circleY);
+            let distanceToAirHockeyCircle = p.dist(p.mouseX, p.mouseY, p.circleAirHockeyX, p.circleAirHockeyY);
+            let distanceToShooterCircle = p.dist(p.mouseX, p.mouseY, p.circleShooterX, p.circleShooterY);
 
-        p.textAlign(p.CENTER, p.CENTER);
-        p.textSize(32);
-        p.fill(0);
-        p.text("AIR HOCKEY", p.circleAirHockeyX, p.circleAirHockeyY);
-        p.fill(0);
-
-        p.textAlign(p.CENTER, p.CENTER);
-        p.textSize(32);
-        p.fill(0);
-        p.text("SHOOTER", p.circleShooterX, p.circleShooterY);
-        p.fill(0);
-        
-        switch (game){
-            case 1: footballGame.update();
-                break;
-            case 2: airHockeyGame.update();
-                break;
-            case 3: shooterGame.update();
+            if (p.mouseIsPressed) {
+                if (distanceToFootballCircle < p.circleSize / 2) {
+                    menuActive = false;
+                    footballGame.resetGame();
+                    game = 1;
+                } else if (distanceToAirHockeyCircle < p.circleSize / 2) {
+                    menuActive = false;
+                    airHockeyGame.resetGame();
+                    game = 2;
+                } else if (distanceToShooterCircle < p.circleSize / 2) {
+                    menuActive = false;
+                    shooterGame.resetGame();
+                    game = 3;
+                }
+            }
+        } else {
+            switch (game) {
+                case 1: 
+                    footballGame.update();
+                    break;
+                case 2: 
+                    airHockeyGame.update();
+                    break;
+                case 3: 
+                    shooterGame.update();
+                    break;
                 default:
-                    //Colisiones Punto circulo y Punto rectangulo
-                    let distanceToFootballCircle = p.dist(p.mouseX, p.mouseY, p.circleX, p.circleY);
-                    let distanceToAirHockeyCircle = p.dist(p.mouseX, p.mouseY, p.circleAirHockeyX, p.circleAirHockeyY);
-                    let distanceToShooterCircle = p.dist(p.mouseX, p.mouseY, p.circleShooterX, p.circleShooterY);
+                    menuActive = true;
+                    game = 0;
+                    break;
+            }
+        }
+    };
 
-                    if (distanceToFootballCircle < p.dotSize / 2 + p.circleSize / 2) {
-                        menuActive = false;
-                        footballGame.resetGame();
-                        game = 1;
-
-                    }else if(distanceToAirHockeyCircle < p.dotSize / 2 + p.circleSize / 2){
-                        menuActive = false;
-                        game = 2;
-                        airHockeyGame.resetGame();
-                    }else if(distanceToShooterCircle < p.dotSize / 2 + p.circleSize / 2){
-                        menuActive = false;
-                        game = 3;
-                        shooterGame.resetGame();
-                    }
-                break;
+    p.keyPressed = function() {
+        if (p.key === 'm' || p.key === 'M') {
+            menuActive = true;
+            game = 0;
         }
     };
 });
--- src/shooter/ShooterGame.js
+++ src/shooter/ShooterGame.js
@@ -1,45 +1,48 @@
-import { FootballBall } from './FootballBall.js';
-import { FootballPlayer } from './FootballPlayer.js';
-import { Goal } from '../shared/Goal.js';
-import { displayScore } from '../shared/GameManager.js';
-import { checkGoals } from '../shared/GameManager.js';
-import { handlePlayers, handleBall } from './FootballInputManager.js';
+import { ShooterPlayer } from './ShooterPlayer.js';
+import { handlePlayer } from './ShooterInputManager.js';
 
-export function shooterGame(p) {
-    this.scores = { score1: 0, score2: 0 };
+export function ShooterGame(p) {
+    this.player1;
+    this.player2;
+    this.winner = null;
 
     this.update = function () {
         p.background(255);
-        this.leftGoal.display(p);
-        this.rightGoal.display(p);
-        handlePlayers(this.player1, this.player2, (p.deltaTime / 1000) * 60, p);
-        handleBall(this.ball, this.player1, this.player2, (p.deltaTime / 1000) * 60, p);
-        checkGoals(this.ball, this.leftGoal, this.rightGoal,this.scores, p);
-        displayScore(this.scores.score1, this.scores.score2,p);
+        if (!this.winner) {
+            handlePlayer(this.player1, p);
+            handlePlayer(this.player2, p);
+            this.checkCollisions();
+        } else {
+            this.displayWinner();
+        }
     };
 
     this.resetGame = function () {
-        p.goalWidth = 10;
-        p.goalHeight = 100;
-        p.goalTop = p.height - p.goalHeight;
-        p.goalBottom = p.height;
-
-
-        this.player1 = new FootballPlayer(50, p.height - 30 / 2, 30, 'blue', p);
-        this.player2 = new FootballPlayer(p.width - 50, p.height - 30 / 2, 30, 'yellow', p);
-
-        this.ball = new FootballBall(p.width / 2, p.height / 2, 5, 5, 20, p);
-
-        this.leftGoal = new Goal(10, p.height - 50, 10, 50, p);
-        this.rightGoal = new Goal(p.width - 20, p.height - 50, 10, 50, p);
+        this.player1 = new ShooterPlayer(100, p.height / 2, 30, 'blue', p, 1);
+        this.player2 = new ShooterPlayer(p.width - 100, p.height / 2, 30, 'green', p, 2);
+        this.winner = null;
     };
 
-    return this
+    this.checkCollisions = function () {
+        this.player1.bullets.forEach(bullet => {
+            if (bullet.checkCollision(this.player2)) {
+                this.player2.hit();
+                this.winner = 'Player 1';
+            }
+        });
+
+        this.player2.bullets.forEach(bullet => {
+            if (bullet.checkCollision(this.player1)) {
+                this.player1.hit();
+                this.winner = 'Player 2';
+            }
+        });
+    };
+
+    this.displayWinner = function () {
+        p.fill(0);
+        p.textSize(32);
+        p.textAlign(p.CENTER, p.CENTER);
+        p.text(this.winner + ' Wins!', p.width / 2, p.height / 2);
+    };
 }
-
-
-
-
-
-
-
